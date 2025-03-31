dad8a97bf5e571611b1ec2ce0b7784715228cf37
karlosvas <126987511+karlosvas@users.noreply.github.com>
2025-04-01 00:21:32 +0200

    chore: initialize Spring Boot project with dependencies

---
+++ src/test/java/com/grupo06/sistemapedidos/SistemapedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupo06.sistemapedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemapedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
